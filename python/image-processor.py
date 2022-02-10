087c35e130ecb2aa10e0b2de527eb5145812f7c1
mats <mats.lovstrand.berntsen@gmail.com>
2022-02-10 18:30:14 +0000

    working scanning and update in db

--- python/image-processor.py
+++ python/image-processor.py
@@ -3,6 +3,7 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
+import requests
 import json
 
 endpoint = "https://ninjas-computer-vision.cognitiveservices.azure.com/"
@@ -10,13 +11,11 @@ config = json.load(open("./config.json", "r"))
 
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(config["computer_vision_key"]))
 
-#ingredients = json.load(open("./ingredients.json", "r"))
+ingredients = json.load(open("./ingredients.json", "r"))
 
 print("start")
 
-rc = call("./capture.sh", shell=True)
-
-print(rc)
+call("./capture.sh", shell=True)
 
 local_image = open("image.jpg", "rb")
 local_image_features = ["objects"]
@@ -27,16 +26,19 @@ tags_result_local = computervision_client.tag_image_in_stream(local_image)
 #print("Tags in the local image: ")
 if (len(tags_result_local.tags) == 0):
     print("No tags detected.")
-#else:
- #   for tag in tags_result_local.tags:
- ##       print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
-print()
 
 matches = []
 
 for tag in tags_result_local.tags:
-    if tag.confidence >= 7:
+    if tag.name in ingredients and tag.confidence > .6:
         matches.append(tag.name)
-
+print()
+print("ingredients in the picture: ")
 for match in matches:
     print(match)
+
+print()
+headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
+
+r = requests.post(config["power_automate_endpoint"], data = json.dumps(matches), headers=headers )
+print(r)
