428df5b7aa7c35fb57d4fde6bd88dc7dfd488129
mats <mats.lovstrand.berntsen@gmail.com>
2022-02-11 13:20:18 +0000

    working stepper and button

---
+++ python/arduino-handler.py
@@ -0,0 +1,49 @@
+import time
+import pyfirmata
+import StepperLib
+import time
+import datetime
+#'COM3' is the USB port mine was plugged into
+board = pyfirmata.Arduino("/dev/ttyACM0") # remember to go to arduino ide, examples, firmata, standard firmata
+reader = pyfirmata.util.Iterator(board)
+reader.start()
+
+
+if __name__ == '__main__':
+    # Initiate communication with Arduino
+    board = pyfirmata.Arduino("/dev/ttyACM0")
+    print("Communication Successfully started")
+    
+    # Create bunch of useful variables
+    button = board.digital[4]
+    
+    button_states = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
+    button_pressed_at = time.time()
+    # Start iterator to receive input data
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+    button.mode = pyfirmata.INPUT
+    index = 0    
+    motor = StepperLib.Stepper(2038, board, reader, 8, 9, 10, 11)
+    motor.set_speed(5)
+
+    while True:
+        motor.step(5)
+        time.sleep(0.01)
+        
+        if index > 9:
+            index = 0
+
+        # Get button current state
+        button_state = button.read()
+        button_states[index] = button_state
+        
+        # Check if button has been released
+        if button_pressed_at < time.time() - 5 and button_state == 0 and all(state == 0 for state in button_states):
+            # and power on next LED
+            print("press")
+            button_pressed_at = time.time()
+            for i in range(0, len(button_states)):
+                button_states[i] = True
+
+        index += 1
---
+++ python/stepper_motor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import pyfirmata
+import time
+import sys
+
+if __name__ == '__main__':
+    # Initiate communication with Arduino
+    board = pyfirmata.Arduino("/dev/ttyACM0")
+    print("Communication Successfully started")
+    
+    # Create bunch of useful variables
+    button = board.digital[4]
+    LED = board.digital[13]
+    
+    button_states = [1, 1, 1, 1, 1, 1, 1, 0, 0, 0] 
+    
+    # Start iterator to receive input data
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+
+    # Setup LEDs and button
+    button.mode = pyfirmata.INPUT
+    previous_button_state = 0
+    index = 0
+    LED.write(0)
+
+    # The "void loop()"
+    while True:
+        # We run the loop at 100Hz
+        time.sleep(0.01)
+        
+        if index > 9:
+            index = 0
+
+        # Get button current state
+        button_state = button.read()
+        button_states[index] = button_state
+        
+        # Check if button has been released
+        if button_state == 0 and all(state == button_state for state in button_states):
+            # and power on next LED
+            print("press")
+            for i in range(0, len(button_states)):
+                button_states[i] = True
+            time.sleep(1)
+        
+        index += 1
+
