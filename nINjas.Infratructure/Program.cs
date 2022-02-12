a6fd0488a092287e3a33eb0da40b143712c3e96e
TommyBakkenMydland <44838160+TommyBakkenMydland@users.noreply.github.com>
2022-02-12 09:14:25 +0100

    pulumi .net 3.1

--- nINjas.Infratructure/Program.cs
+++ nINjas.Infratructure/Program.cs
@@ -1,7 +1,14 @@
-﻿
-using Pulumi;
-class program
-{
-    static Task<int> Main() => Deployment.RunAsync<AppServiceStack>();
-}
+﻿using Pulumi;
+using System;
+using System.Threading.Tasks;
 
+namespace nINjas.Infratructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static Task<int> Main() => Deployment.RunAsync<AppServiceStack>();
+        }
+    }
+}
